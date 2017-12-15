59fecd9f3870a82c199402716f25c9e86f761c42
Christine Snyman <christinesnyman96@gmail.com>
2017-12-15 13:30:36 +0000

    Add project files.

---
+++ ShootShapesUp/GameRoot.cs
@@ -0,0 +1,120 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using System;
+using System.Linq;
+
+namespace ShootShapesUp
+{
+    public class GameRoot : Game
+    {
+        // some helpful static properties
+        public static GameRoot Instance { get; private set; }
+        public static Viewport Viewport { get { return Instance.GraphicsDevice.Viewport; } }
+        public static Vector2 ScreenSize { get { return new Vector2(Viewport.Width, Viewport.Height); } }
+        public static GameTime GameTime { get; private set; }
+
+        public static Texture2D Player { get; private set; }
+        public static Texture2D Seeker { get; private set; }
+        public static Texture2D Bullet { get; private set; }
+        public static Texture2D Pointer { get; private set; }
+
+        public static SpriteFont Font { get; private set; }
+
+        public static Song Music { get; private set; }
+
+        private static readonly Random rand = new Random();
+
+        private static SoundEffect[] explosions;
+        // return a random explosion sound
+        public static SoundEffect Explosion { get { return explosions[rand.Next(explosions.Length)]; } }
+
+        private static SoundEffect[] shots;
+        public static SoundEffect Shot { get { return shots[rand.Next(shots.Length)]; } }
+
+        private static SoundEffect[] spawns;
+        public static SoundEffect Spawn { get { return spawns[rand.Next(spawns.Length)]; } }
+
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        public GameRoot()
+        {
+            Instance = this;
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = @"Content";
+
+            graphics.PreferredBackBufferWidth = 800;
+            graphics.PreferredBackBufferHeight = 600;
+        }
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+
+            EntityManager.Add(PlayerShip.Instance);
+
+            MediaPlayer.IsRepeating = true;
+            MediaPlayer.Play(GameRoot.Music);
+        }
+
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            Player = Content.Load<Texture2D>("Art/Player");
+            Seeker = Content.Load<Texture2D>("Art/Seeker");
+            Bullet = Content.Load<Texture2D>("Art/Bullet");
+            Pointer = Content.Load<Texture2D>("Art/Pointer");
+
+            Font = Content.Load<SpriteFont>("Font");
+
+            Music = Content.Load<Song>("Sound/Music");
+
+            // These linq expressions are just a fancy way loading all sounds of each category into an array.
+            explosions = Enumerable.Range(1, 8).Select(x => Content.Load<SoundEffect>("Sound/explosion-0" + x)).ToArray();
+            shots = Enumerable.Range(1, 4).Select(x => Content.Load<SoundEffect>("Sound/shoot-0" + x)).ToArray();
+            spawns = Enumerable.Range(1, 8).Select(x => Content.Load<SoundEffect>("Sound/spawn-0" + x)).ToArray();
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            GameTime = gameTime;
+            Input.Update();
+
+            // Allows the game to exit
+            if (Input.WasButtonPressed(Buttons.Back) || Input.WasKeyPressed(Keys.Escape))
+                this.Exit();
+
+            EntityManager.Update();
+            EnemySpawner.Update();
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Black);
+
+            // Draw entities. Sort by texture for better batching.
+            spriteBatch.Begin(SpriteSortMode.Texture, BlendState.Additive);
+            EntityManager.Draw(spriteBatch);
+            spriteBatch.End();
+
+            // Draw user interface
+            spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.Additive);
+
+            spriteBatch.End();
+
+
+            base.Draw(gameTime);
+        }
+
+        private void DrawRightAlignedString(string text, float y)
+        {
+            var textWidth = GameRoot.Font.MeasureString(text).X;
+            spriteBatch.DrawString(GameRoot.Font, text, new Vector2(ScreenSize.X - textWidth - 5, y), Color.White);
+        }
+    }
+}
